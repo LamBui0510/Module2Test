5d56da638a5eb25a2e35c8577e402dffdc481a10
LamBui0510 <buitunglamvnpt@gmail.com>
2021-12-10 11:22:36 +0700

    file module2 last3

--- src/service/Regex.java
+++ src/service/Regex.java
@@ -7,15 +7,8 @@ import java.util.regex.Pattern;
 public class Regex {
     private static Pattern pattern;
     private static Matcher matcher;
-    public static final String ACCOUNT = "^[A-Za-z0-9]{6,}$";
-    public static final String PASS = "^[A-z0-9A-z]{6,}$";
     public static final String NUMBER = "^[0-9]+$";
-    public static final String PHONE = "^[0-9]{8}+$";
-    public static final String DOUBLE = "^(\\d+\\.\\d+)|\\d+";
-    public static final String NAME ="^\\pL+[\\pL\\pZ\\pP]{0,}$";
     public static final String EMAIL="^[A-Za-z0-9]+[A-Za-z0-9]*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)$";
-    public static final String STATUS="^AVAILABLE|READY|CLEANING$";
-    public static final String ROOMTYPE="^VIP||DELUX||PREMIUM$";
     static Scanner sc = new Scanner(System.in);
 
     public static boolean validateAll(String value,String regex) {
